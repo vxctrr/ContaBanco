b6a36e676212836623d27ea0c76ddf673c607760
Victor <vctorgpassi@gmail.com>
2024-08-14 22:38:00 -0300

    Finalizando Projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Testando repositorio");
+       
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = sc.next();
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = sc.next();
+        System.out.println("Digite seu saldo: ");
+        double saldoConta = sc.nextDouble();
+        System.out.println("Digite o numero da sua conta: ");
+        int numeroConta = sc.nextInt();
+
+        String mensagem = "Olá ".concat(nomeCliente)
+                .concat(", obrigado por criar uma conta em nosso banco, sua agência é: ")
+                .concat(agencia)
+                .concat(" e sua conta: ")
+                .concat(String.valueOf(numeroConta))
+                .concat(", seu saldo é de R$")
+                .concat(String.valueOf(saldoConta))
+                .concat(" e já está disponível para saque!");
+
+        System.out.println(mensagem);
+        sc.close();
     }
 }
